f6f006dd5d9dae763350c1f49c4964f13734f55e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-05-15 00:39:45 +0800

    intro graph library
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- Minic/Passes/InferGraph.lean
+++ Minic/Passes/InferGraph.lean
@@ -1,19 +1,22 @@
--- import Mathlib.Combinatorics.SimpleGraph.Basic
+import Graph
 import Minic.Passes.LivenessAnalysis
 
 namespace Minic.Passes
+open Graph
 open Minic.IR.Arm64
 
 structure Instr3Block (instr : Type) extends (Instr2Block instr) where
-  -- conflictGraph : SimpleGraph Reg
+  -- `UndirectedGraph` type takes payload type & weight type
+  -- we don't need weights, so we use `Unit`
+  conflictGraph : UndirectedGraph Reg Unit
 instance [ToString instr] : ToString (Instr3Block instr) where
-  -- TODO: print conflict graph
-  toString _ := "built conflict graph: "
+  toString b := s!"built conflict graph: {b.conflictGraph}"
 
 end Minic.Passes
 
 namespace Minic.Passes.InferGraph
 open Std
+open Graph.UndirectedGraph
 open Minic.Ast
 open Minic.IR.Asm
 open Minic.IR.Arm64
@@ -31,8 +34,10 @@ def inferGraphBlock (block : Instr2Block Arm64Instr) : Instr3Block Arm64Instr :=
   let edges := block.livenessSets.zipWith block.instructions.toArray compute
     |> Array.toList
     |> List.join
+    |> List.toArray
+  let g := .empty
   -- TODO: build graph from edges
-  { block with }
+  { block with conflictGraph := g }
 
 def pass (p : AsmProg $ Instr2Block Arm64Instr) : AsmProg $ Instr3Block Arm64Instr := Id.run do
   let mut blocks' : List (String × Instr3Block Arm64Instr) := []
--- lakefile.lean
+++ lakefile.lean
@@ -13,6 +13,9 @@ require std from git
   "https://github.com/leanprover/std4.git"
   @"fe45c329180f4d051f9f19d1a360d9e195532afb"
 
+require graph from git
+  "https://github.com/PeterKementzey/graph-library-for-lean4"
+
 @[default_target]
 lean_exe «minic» {
   root := `Main
