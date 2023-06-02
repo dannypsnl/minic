644f6d04ed224bee7e6390e88a0d515fdd9f17ea
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-06-03 00:22:16 +0800

    remove unused instance
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- Minic/Ast.lean
+++ Minic/Ast.lean
@@ -3,15 +3,6 @@ namespace Minic.Ast
 inductive MOp | add | sub | mul | div
 deriving Repr, BEq, Inhabited
 
-instance : Coe Char (Option MOp) where
-  coe c :=
-  match c with
-  | '+' => .some .add
-  | '-' => .some .sub
-  | '*' => .some .mul
-  | '/' => .some .div
-  | _ => .none
-
 inductive MExpr
   | symbol (x : String)
   | fixnum (v : Int)
