9c1b04fd8657bb6a07bfd4d7717ff0f87fdb706c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-07-22 22:53:28 +0800

    draft sexp to expr
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Parsexp_io
 
-let () =  
-  let filename = Sys.argv.(1) in 
-  let sexp = load_conv_exn Single ~filename: filename (fun a -> a) in
+let () =
+  let filename = Sys.argv.(1) in
+  let sexp = load_conv_exn Single ~filename (fun a -> a) in
   print_endline (Base.Sexp.to_string sexp)
---
+++ lib/ast.ml
@@ -0,0 +1,29 @@
+open Base
+open List
+
+exception Unhandled_sexp of Sexp.t
+
+type expr =
+  | Int of int
+  | Var of string (* (+ 1 2 3) *)
+  | Prim of op * expr list (* (let ([x t]) u) *)
+  | Let of string * expr * expr
+[@@deriving show]
+
+and op = Add | Sub [@@deriving show]
+
+let rec expr_from_sexp : Sexp.t -> expr =
+ fun se ->
+  match se with
+  | List (Atom "+" :: rest) -> Prim (Add, map rest ~f:expr_from_sexp)
+  | List (Atom "-" :: rest) -> Prim (Sub, map rest ~f:expr_from_sexp)
+  | List [ Atom "let"; List [ Atom x; t ]; u ] ->
+      Let (validate_varname x, expr_from_sexp t, expr_from_sexp u)
+  | Atom x -> (
+      match Int.of_string_opt x with
+      | Some i -> Int i
+      | None -> Var (validate_varname x))
+  | List _ -> raise (Unhandled_sexp se)
+
+(* TODO: raise exception for bad name like `123` *)
+and validate_varname : string -> string = fun x -> x
