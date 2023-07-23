2c6021137fdef4f3934cd6add7331207078e7f9e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-07-23 19:41:07 +0800

    use Base as possible
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,21 @@
+open Base
 open Parsexp_io
 open Minic.Ast
 
 let () =
-  let filename = Sys.argv.(1) in
+  let filename = (Sys.get_argv ()).(1) in
   let e = load_conv_exn Single ~filename (fun e -> e) in
-  print_endline "stage 0";
-  print_endline ([%derive.show: Base.Sexp.t] e);
+  Stdio.print_endline "stage 0";
+  Stdio.print_endline (Sexp.to_string e);
   let e = e |> expr_from_sexp in
-  print_endline "\nstage 1";
-  print_endline ([%derive.show: expr] e);
+  Stdio.print_endline "\nstage 1";
+  Stdio.print_endline ([%derive.show: expr] e);
   let e =
     e |> Minic.Pass_uniquify.run |> Minic.Pass_remove_complex_operands.run
     |> Minic.Pass_explicate_control.run
   in
-  print_endline "\nstage 3";
-  print_endline (show_ctail e);
+  Stdio.print_endline "\nstage 3";
+  Stdio.print_endline (show_ctail e);
   let e = e |> Minic.Pass_select_instruction.run in
-  print_endline "\nstage 4";
-  print_endline (show_asm e)
+  Stdio.print_endline "\nstage 4";
+  Stdio.print_endline (show_asm e)
