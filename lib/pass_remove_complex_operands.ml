8e29b31e2752fa7d67205c7a2a1a4a785d43eba2
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-07-24 19:38:38 +0800

    fix rco pass
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- lib/pass_remove_complex_operands.ml
+++ lib/pass_remove_complex_operands.ml
@@ -2,34 +2,36 @@ open Ast
 
 let rec run : expr -> expr =
  fun e ->
-  let temp_var_cnt = ref 1 in
-  rco_expr temp_var_cnt e
+  temp_var_cnt := 1;
+  rco_expr e
 
-and rco_atom : int ref -> expr -> expr * (string * expr) list =
- fun temp_var_cnt e ->
+and rco_atom : expr -> expr * (string * expr) list =
+ fun e ->
   match e with
   | Var x -> (Var x, [])
   | Int i -> (Int i, [])
   | Prim (op, es) ->
-      let es' = List.map (rco_atom temp_var_cnt) es in
+      let es' = List.map rco_atom es in
       let atoms, bb = List.split es' in
       let temp_binder = "tmp." ^ Int.to_string !temp_var_cnt in
       temp_var_cnt := !temp_var_cnt + 1;
       (Var temp_binder, (temp_binder, Prim (op, atoms)) :: List.concat bb)
   | Let (x, t, body) ->
-      (Let (x, rco_expr temp_var_cnt t, rco_expr temp_var_cnt body), [])
+      let atom_body, bindings = rco_atom body in
+      (atom_body, (x, rco_expr t) :: bindings)
 
-and rco_expr : int ref -> expr -> expr =
- fun temp_var_cnt e ->
+and rco_expr : expr -> expr =
+ fun e ->
   match e with
   | Var _ | Int _ -> e
   | Prim (op, es) ->
-      let es' = List.map (rco_atom temp_var_cnt) es in
+      let es' = List.map rco_atom es in
       let atoms, bb = List.split es' in
       let bindings = List.concat bb in
-      List.fold_right
-        (fun (x, e) body -> Let (x, e, body))
-        bindings
+      List.fold_left
+        (fun body (x, e) -> Let (x, e, body))
         (Prim (op, atoms))
-  | Let (x, t, body) ->
-      Let (x, rco_expr temp_var_cnt t, rco_expr temp_var_cnt body)
+        bindings
+  | Let (x, t, body) -> Let (x, rco_expr t, rco_expr body)
+
+and temp_var_cnt = ref 1
