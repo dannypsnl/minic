525416ebffbc3854d510c644160ebf92449944c0
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-07-24 13:34:27 +0800

    remove ocamlgraph dependency
    Well, I cannot figure out how to construct a simple graph from this
    library...
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- lib/pass_graph_inference.ml
+++ lib/pass_graph_inference.ml
@@ -1,21 +1,7 @@
 open Ast
-open Graph
-
-module IntInt = struct
-  type t = int * int
-end
-
-module Int = struct
-  type t = int
-
-  let compare = compare
-  let hash = Hashtbl.hash
-  let equal = ( = )
-  let default = 0
-end
-
-module G = Imperative.Graph.AbstractLabeled (IntInt) (Int)
 
 exception TODO
 
-let run : asm -> asm * G.t = fun _ -> raise TODO
+type graph
+
+let run : asm -> asm * graph = fun _ -> raise TODO
