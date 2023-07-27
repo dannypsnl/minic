3a66fbb24095fcd78eaee6c6ef6fdbfa4dae677f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-07-27 16:45:54 +0800

    improve naming
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- lib/pass_stack_patch.ml
+++ lib/pass_stack_patch.ml
@@ -1,9 +1,5 @@
 open Ast
 
-let add (d, s1, s2) = Add (d, s1, s2)
-and sub (d, s1, s2) = Sub (d, s1, s2)
-and xor (d, s1, s2) = Xor (d, s1, s2)
-
 let rec run : debug:int -> asm -> asm =
  fun ~debug prog ->
   let prog = prog |> List.map patch_instruction |> List.concat in
@@ -20,31 +16,34 @@ and patch_instruction : instruction -> asm = function
         (patch_src (`Reg "x28") (`Sp i))
         (patch_instruction (Mov (d, `Reg "x28")))
   | Mov (d, s) -> [ Mov (d, s) ]
-  | Add (`Sp i, s1, s2) -> f add i s1 s2
-  | Add (d, `Sp i, s2) -> f2 add d i s2
-  | Add (d, s1, `Sp i) -> f3 add d s1 i
+  | Add (`Sp i, s1, s2) -> patch_instr_dest add i s1 s2
+  | Add (d, `Sp i, s2) -> patch_instr_s1 add d i s2
+  | Add (d, s1, `Sp i) -> patch_instr_s2 add d s1 i
   | Add (d, s1, s2) -> [ Add (d, s1, s2) ]
-  | Sub (`Sp i, s1, s2) -> f sub i s1 s2
-  | Sub (d, `Sp i, s2) -> f2 sub d i s2
-  | Sub (d, s1, `Sp i) -> f3 sub d s1 i
+  | Sub (`Sp i, s1, s2) -> patch_instr_dest sub i s1 s2
+  | Sub (d, `Sp i, s2) -> patch_instr_s1 sub d i s2
+  | Sub (d, s1, `Sp i) -> patch_instr_s2 sub d s1 i
   | Sub (d, s1, s2) -> [ Sub (d, s1, s2) ]
-  | Xor (`Sp i, s1, s2) -> f xor i s1 s2
-  | Xor (d, `Sp i, s2) -> f2 xor d i s2
-  | Xor (d, s1, `Sp i) -> f3 xor d s1 i
+  | Xor (`Sp i, s1, s2) -> patch_instr_dest xor i s1 s2
+  | Xor (d, `Sp i, s2) -> patch_instr_s1 xor d i s2
+  | Xor (d, s1, `Sp i) -> patch_instr_s2 xor d s1 i
   | Xor (d, s1, s2) -> [ Xor (d, s1, s2) ]
   | i -> [ i ]
 
-and f : (reg * src * src -> instruction) -> int -> src -> src -> asm =
+and patch_instr_dest :
+    (reg * src * src -> instruction) -> int -> src -> src -> asm =
  fun c i s1 s2 ->
   List.append (patch_instruction (c (`Reg "x28", s1, s2))) (patch_dest (`Sp i))
 
-and f2 : (reg * src * src -> instruction) -> reg -> int -> src -> asm =
+and patch_instr_s1 :
+    (reg * src * src -> instruction) -> reg -> int -> src -> asm =
  fun c d i s2 ->
   List.append
     (patch_src (`Reg "x28") (`Sp i))
     (patch_instruction (c (d, `Reg "x28", s2)))
 
-and f3 : (reg * src * src -> instruction) -> reg -> src -> int -> asm =
+and patch_instr_s2 :
+    (reg * src * src -> instruction) -> reg -> src -> int -> asm =
  fun c d s1 i ->
   List.append
     (patch_src (`Reg "x27") (`Sp i))
