6b4dc618a8726b6c8adde2345f783c011b1235e3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-08-15 23:34:07 +0800

    compact main
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,11 @@
-open Minic.Compiler
-open Eio
-
 let () =
+  let open Eio in
   Eio_main.run @@ fun env ->
   (* debug range: 0 ~ 3 *)
   let debug = 3 in
   let filename = (Base.Sys.get_argv ()).(1) in
   let output =
-    compile ~debug
+    Minic.Compiler.compile ~debug
       ~proc_mgr:(Eio.Stdenv.process_mgr env)
       ~cwd:(Eio.Stdenv.cwd env) ~filename
   in
