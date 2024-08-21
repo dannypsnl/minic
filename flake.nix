1fb43248e3991f32d69721185137c7e1becb29cc
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-21 19:34:49 +0800

    add flake.nix
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    opam-repository.url = "github:ocaml/opam-repository";
+    opam-repository.flake = false;
+    opam-nix.url = "github:tweag/opam-nix";
+  };
+  outputs = { self, flake-utils, opam-nix, nixpkgs, opam-repository }@inputs:
+    let package = "minic";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-base-compiler = "5.1.1";
+          ocaml-lsp-server = "*";
+        };
+        query = devPackagesQuery // { };
+        scope =
+          on.buildOpamProject' { repos = [ "${opam-repository}" ]; } ./. query;
+        overlay = final: prev: {
+          ${package} =
+            prev.${package}.overrideAttrs (_: { doNixSupport = false; });
+        };
+        scope' = scope.overrideScope' overlay;
+        main = scope'.${package};
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in {
+        legacyPackages = scope';
+        packages.default = main;
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages;
+        };
+      });
+}
